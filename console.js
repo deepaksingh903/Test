a66cef8283494f0c26cc8ed60b0217b0c45839a4
dpk903 <deepaksingh903@gmail.com>
2018-04-09 19:27:59 +0530

    Create console.js

---
+++ console.js
@@ -0,0 +1,6 @@
+var x=document.getElementsByClassName("paywall");
+x[0].style.display = 'none';
+var y=document.getElementsByClassName("vjs-play-control-play");
+y[0].id =  "deepak";
+var z=y[0];
+window.setInterval(function() { z.click(); }, 30);
